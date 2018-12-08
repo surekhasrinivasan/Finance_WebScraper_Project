2a1bd33330e33dcad24914e874baebe579241e84
surekhasrinivasan <surekhasrao@gmail.com>
2018-12-08 06:28:14 -0500

    Customized HomeController.cs and its Views

--- Finance_WebScraper/Controllers/HomeController.cs
+++ Finance_WebScraper/Controllers/HomeController.cs
@@ -17,14 +17,14 @@ namespace Finance_WebScraper.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Application description page.";
 
             return View();
         }
 
         public IActionResult Contact()
         {
-            ViewData["Message"] = "Your contact page.";
+            ViewData["Message"] = "My Contact Info";
 
             return View();
         }
