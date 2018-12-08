81c63a01877386b41ec7edca8ea23e3fc8e7853a
surekhasrinivasan <surekhasrao@gmail.com>
2018-12-08 06:48:00 -0500

    Add Migration and Update Database

--- Finance_WebScraper/Data/Migrations/20181208114028_Initial.Designer.cs
+++ Finance_WebScraper/Data/Migrations/20181208114028_Initial.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace Finance_WebScraper.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20181208114028_Initial")]
+    partial class Initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
